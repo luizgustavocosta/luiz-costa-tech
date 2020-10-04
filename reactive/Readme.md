a869de698f6606bcf37950d308ad0dd5a0fab42e
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2020-10-04 15:19:31 +0200

    Initial reactive project and agenda.

---
+++ reactive/Readme.md
@@ -0,0 +1,9 @@
+# Agenda
+
+## Reactive in a nutshell
+
+## Working with Publisher and Subscriber
+
+## Design and run an api reactive
+
+## Consumes the api using a UI 
---
+++ reactive/src/main/java/tech/costa/luiz/reactive/ReactiveApplication.java
@@ -0,0 +1,13 @@
+package tech.costa.luiz.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApplication.class, args);
+	}
+
+}
